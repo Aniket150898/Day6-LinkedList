785e22342492305d5c457387542bb233dd7b3a59
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-23 19:42:34 +0530

    [Aniket]- create a linked list to add data

---
+++ LinkedListProblem/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        internal Node head;
+
+        //creating add method to add the data in linked list sequentially
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into the linked list", node.data);
+        }
+        //displaying the data from linked list
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace LinkedListProblem
+﻿using System.Collections.Generic;
+
+namespace LinkedListProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Problem");
+            Console.WriteLine("\n1.CreateLinkedList");
+            LinkedList list = new LinkedList();
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Creating a linked list to add data");
+                    list.Add(56);
+                    list.Add(30);
+                    list.Add(70);
+                    list.Display();
+                    break;
+                default:
+                    Console.WriteLine("Please enter correct option ");
+                    break;
+            }
         }
     }
 }
