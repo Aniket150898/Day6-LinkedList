1e6186834227ec6a28f1a474608a9b83b96e78cb
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-03-23 20:44:56 +0530

    [Aniket]- delete the last element in linked list

--- LinkedListProblem/LinkedList.cs
+++ LinkedListProblem/LinkedList.cs
@@ -89,6 +89,22 @@ namespace LinkedListProblem
             }
         }
 
+        //deleting last element from linked list
+        internal Node RemoveLastNode()
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return null;
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+
         //displaying the data from linked list
         internal void Display()
         {
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -7,7 +7,8 @@ namespace LinkedListProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List Problem");
-            Console.WriteLine("\n1.CreateLinkedList \n2.AddFirstLinkedList \n3.AppendLinkedList \n4.InsertLinkedList \n5.DeleteFirstElement");
+            Console.WriteLine("\n1.CreateLinkedList \n2.AddFirstLinkedList \n3.AppendLinkedList \n4.InsertLinkedList \n5.DeleteFirstElement" +
+                             "\n6.DeleteLastElement");
             LinkedList list = new LinkedList();
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -47,6 +48,16 @@ namespace LinkedListProblem
                     list.Append(70);
                     list.Display();
                     list.RemoveFirstNode();
+                    list.Display();
+                    break;
+                case 6:
+                    Console.WriteLine("Delete last element in linked list ");
+                    list.Append(56);
+                    list.Append(30);
+                    list.Append(70);
+                    list.Display();
+                    list.RemoveLastNode();
+                    Console.WriteLine("Remove last node from linked list ");
                     list.Display();
                     break;
                 default:
